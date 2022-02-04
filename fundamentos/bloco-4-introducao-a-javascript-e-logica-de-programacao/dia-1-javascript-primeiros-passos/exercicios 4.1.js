0c8adfe25cc523002dabee18e41453640193667c
luis fernando cutilak <luiscutilak@gmail.com>
2022-02-04 08:50:17 -0300

    Exercicio 10, concluido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios 4.1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios 4.1.js
@@ -138,3 +138,17 @@
 //   console.log(true);
 // }else console.log(false);
 
+// 9.Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+// const num1 = 47;
+// const num2 = 34;
+// const num3 = 12;
+
+// if (num2 %2 !== 0) {
+//   console.log(true)  
+// }else console.log(false);
+
+
+
+
