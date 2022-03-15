437cd6a1925b40e9b0604b10f4e328803a3fde13
luis fernando cutilak <luiscutilak@gmail.com>
2022-03-15 09:42:59 -0300

    Exercicio 4, ok

---
+++ fundamentos/dia-4-javascript-es6-higher-order-functions-reduce/exercicio 4.js
@@ -0,0 +1,84 @@
+// 4 - Encontre o livro com o maior nome.
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+
+//   4 - Encontre o livro com o maior nome.
+
+function longestNamedBook() {
+return books.reduce((biggestBook, currentBook) => ( // Nessa linha o reduce filtra os livros e acumula em biggestBook, enquanto os valores atuais vao "caindo em current book"
+currentBook.name.length > biggestBook.name.length) // Nessa linha é feita uma condição, onde, Se o comprimento do livro for MAIOR que o Livro que esta no ACUMULADOr
+? currentBook : biggestBook) // RETORNE O LIVRO ATUAL, CASO CONTRARIO RETORNE O LIVRE DO ACUMULADOR.
+}
+
+console.log(longestNamedBook());
